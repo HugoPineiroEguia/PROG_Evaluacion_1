5d5d92b558e979c32a6c92cb1233724c7a842b23
Hugo Piñeiro Eguía <hpineiroeguia@danielcastelao.org>
2021-10-19 17:38:35 +0200

    Boletín 5 terminado

--- src/Boletin5/Boletin5.java
+++ src/Boletin5/Boletin5.java
@@ -1,57 +1,63 @@
 package Boletin5;
 
-
 import java.util.Scanner;
 
 public class Boletin5 {
 
-
     public static void main(String[] args) {
 
         Consumo gastos = new Consumo();
+        System.out.println("----------------------------------");
         gastos.mostrarGastos();
 
+        Consumo gastos2 = new Consumo(13, 100f, 60f, 57f, 1.57f);
+        System.out.println("----------------------------------");
+        gastos2.mostrarGastos();
     }
+
 
     public static class Consumo {
 
-        int tiempo;
-        float consumoMedio;
-        float consumoEuros;
         float km;
         float litros;
         float vMed;
         float pGas;
 
-        public Consumo (int getTiempo, float consumoMedio, float consumoEuros, float setKms, float setLitros, float setvMed, float setPGas){
+        public Consumo(int getTiempo, float setKms, float setLitros, float setvMed, float setPGas) {
 
-
+            km = setKms;
+            litros = setLitros;
+            vMed = setvMed;
+            pGas = setPGas;
 
         }
 
-        public Consumo(){
+        public Consumo() {
 
             Scanner sc = new Scanner(System.in);
-            System.out.print("Tiempo en horas: "); tiempo = sc.nextInt();
-            System.out.print("Consumo medio del vehiculo: "); consumoMedio = sc.nextFloat();
-            System.out.print("Consumo medio en € por cada 100km: "); consumoEuros = sc.nextFloat();
-            System.out.print("Distancia en Km: "); km = sc.nextFloat();
-            System.out.print("Litros de combustible consumido: "); litros = sc.nextFloat();
-            System.out.print("Velocidad media: "); vMed = sc.nextFloat();
-            System.out.print("Precio del gas: "); pGas = sc.nextFloat();
+            System.out.print("Distancia en Km: ");
+            km = sc.nextFloat();
+            System.out.print("Litros de combustible consumido: ");
+            litros = sc.nextFloat();
+            System.out.print("Velocidad media: ");
+            vMed = sc.nextFloat();
+            System.out.print("Precio del gas: ");
+            pGas = sc.nextFloat();
 
         }
 
-        public void mostrarGastos(){
+        public void mostrarGastos() {
 
+            float gastosEuros = (pGas * litros);
+            float consumoMedio = (litros / (km));
+            float consumoEuros = (gastosEuros);
 
-            float gastoFinal;
-
-
-
-            System.out.print("El precio total es: ");
+            System.out.println("El consumo medio es: " + consumoMedio + " litros.");
+            System.out.println("El consumo total es: " + consumoEuros + " €.");
+            System.out.println("La velocidad media es: " + vMed + " Km/h");
 
         }
+
 
 
     }
