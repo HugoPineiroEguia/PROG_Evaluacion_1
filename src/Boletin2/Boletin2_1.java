b3db74a3baaa8c665a61cc90baa64edf05f0afae
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2021-10-19 09:26:18 +0200

    Initial commit

---
+++ src/Boletin2/Boletin2_1.java
@@ -0,0 +1,13 @@
+package Boletin2;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+        float b = 4;
+        float h = 3;
+        float area = b * h / 2;
+        System.out.println("El area del triangulo es " + area + ".");
+
+
+    }
+}
