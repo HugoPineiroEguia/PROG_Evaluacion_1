010c0a656235050aa94644163bda7fd9d2676bef
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2021-10-20 12:26:20 +0200

    Boletin5, con  "Get" y "Set"

--- src/Boletin5/Boletin5.java
+++ src/Boletin5/Boletin5.java
@@ -8,45 +8,84 @@ public class Boletin5 {
 
         Consumo gastos = new Consumo();
         System.out.println("----------------------------------");
-        gastos.mostrarGastos();
+        gastos.setvMed(18f);
+        gastos.setKm(100f);
+        gastos.setPGas(84f);
+        gastos.setLitros(50f);
+        System.out.println("Velocidad media: "+gastos.getVMed());
+        System.out.println("El tiempo empleado en realizar el trayecto ha sido de: "+ gastos.getTiempo());
 
-        Consumo gastos2 = new Consumo(13, 100f, 60f, 57f, 1.57f);
+        Consumo gastos2 = new Consumo(100f, 60f, 50f, 1.57f);
         System.out.println("----------------------------------");
-        gastos2.mostrarGastos();
-    }
+        gastos2.mostrarConsumo();
 
+        Consumo gastos3 = new Consumo();
+        System.out.println("----------------------------------");
+        gastos3.crearConsumo();
+        System.out.println("----------------------------------");
+        gastos3.mostrarConsumo();
+
+    }
 
     public static class Consumo {
 
-        float km;
-        float litros;
-        float vMed;
-        float pGas;
+        private float km;
+        private float litros;
+        private float vMed;
+        private float pGas;
 
-        public Consumo(int getTiempo, float setKms, float setLitros, float setvMed, float setPGas) {
+        public Consumo(float kms, float litros, float vMed, float pGas) {
 
-            km = setKms;
-            litros = setLitros;
-            vMed = setvMed;
-            pGas = setPGas;
+            this.km = kms;
+            this.litros = litros;
+            this.vMed = vMed;
+            this.pGas = pGas;
 
         }
 
         public Consumo() {
 
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Distancia en Km: ");
-            km = sc.nextFloat();
-            System.out.print("Litros de combustible consumido: ");
-            litros = sc.nextFloat();
-            System.out.print("Velocidad media: ");
-            vMed = sc.nextFloat();
-            System.out.print("Precio del gas: ");
-            pGas = sc.nextFloat();
+        }
+
+        public void setvMed(float vMed) {
+
+            this.vMed = vMed;
 
         }
 
-        public void mostrarGastos() {
+        public void setKm(float km) {
+
+            this.km = km;
+
+        }
+
+        public void setLitros(float litros) {
+
+            this.litros = litros;
+
+        }
+
+        public void setPGas(float pGas) {
+
+            this.pGas = pGas;
+
+        }
+
+        public float getVMed(){
+
+            return vMed;
+
+        }
+
+        public float getTiempo(){
+
+            float tiempo = (km/vMed);
+            return tiempo;
+
+        }
+
+
+        public void mostrarConsumo() {
 
             float gastosEuros = (pGas * litros);
             float consumoMedio = (litros / (km));
@@ -58,6 +97,19 @@ public class Boletin5 {
 
         }
 
+
+        public void crearConsumo(){
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Distancia en Km: ");
+            km = sc.nextFloat();
+            System.out.print("Litros de combustible consumido: ");
+            litros = sc.nextFloat();
+            System.out.print("Velocidad media: ");
+            vMed = sc.nextFloat();
+            System.out.print("Precio del gas: ");
+            pGas = sc.nextFloat();
+        }
 
 
     }
